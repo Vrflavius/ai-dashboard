f692f92312abca084a1f3ffe60c0325a10e768c3
Vrflavius <flavius.vranau@smartboxdigital.com>
2024-11-04 16:33:04 +0200

    log-out button in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LoginService } from '../Login/login.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,4 +11,13 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   styleUrls: ['./header.component.scss'],
   imports: [MatSlideToggleModule]
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  ngOnInit() { }
+
+  onLogout(){
+    this.loginService.logoutUser(this.loginService.currenLogUser);
+    this.router.navigate(['login'])
+  }
+}
